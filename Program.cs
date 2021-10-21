e4f80f739b31525b12da57a05e8a064de2f3bedf
bySTEEKman <62076247+bySTEEKman@users.noreply.github.com>
2021-10-22 00:46:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace birthdays
 {
-    class Dictionary
-    {
-        private Dictionary<string, Employee> dict = new Dictionary<string, Employee>();
-        public Dictionary<string, Employee> getDictionary()
-        {
-            return dict;
-        }
-        public void addItems(List<Employee> list)
-        {
-            foreach (Employee employ in list)
-            {
-                string key = employ.birthday.Month.ToString("");
-                if (dict.ContainsKey(key){
-                    dict[k]
-                    }
-                dict.Add(key, employ);
-
-            }
-        }
-    }
     class Employee
     {
         public string name;
@@ -37,21 +17,34 @@ namespace birthdays
     {
         static void Main(string[] args)
         {
-            Dictionary names = new Dictionary();
+            Dictionary<string, List<Employee>> employeeData = new Dictionary<string, List<Employee>>();
             DateTime date = DateTime.Now;
-            List<Employee> empList = new List<Employee>();
-            empList.Add(new Employee("Yan Hurov", new DateTime(2000, 04, 01)));
-            empList.Add(new Employee("Ivan Budko", new DateTime(2002, 05, 01)));
-            empList.Add(new Employee("Ayn Rand", new DateTime(1990, 04, 20)));
-            empList.Add(new Employee("Geralt Rivia", new DateTime(1995, 05, 07)));
-            empList.Add(new Employee("Jameson Brody", new DateTime(2000, 06, 27)));
-            empList.Add(new Employee("Dustin Poirier", new DateTime(2005, 06, 12)));
-            empList.Add(new Employee("Ray Bradbury", new DateTime(1999, 04, 02)));
-            empList.Add(new Employee("Oleh Baibuz", new DateTime(2000, 07, 01)));
-            names.addItems(empList);
-            foreach (KeyValuePair<string, Employee> value in names.getDictionary())
+            List<Employee> employeeList = new List<Employee>();
+            employeeList.Add(new Employee("Yan Hurov", new DateTime(2000, 04, 01)));
+            employeeList.Add(new Employee("Ivan Budko", new DateTime(2002, 05, 01)));
+            employeeList.Add(new Employee("Ayn Rand", new DateTime(1990, 04, 20)));
+            employeeList.Add(new Employee("Geralt Rivia", new DateTime(1995, 05, 07)));
+            employeeList.Add(new Employee("Jameson Brody", new DateTime(2000, 06, 27)));
+            employeeList.Add(new Employee("Dustin Poirier", new DateTime(2005, 06, 12)));
+            employeeList.Add(new Employee("Ray Bradbury", new DateTime(1999, 04, 02)));
+            employeeList.Add(new Employee("Oleh Baibuz", new DateTime(2000, 07, 01)));
+            SortDictionary(employeeData, employeeList);
+        }
+        static public void SortDictionary(Dictionary<string, List<Employee>> data, List<Employee> value)
+        {
+            foreach(Employee human in value)
             {
-                Console.WriteLine($"{value.Value.name}: {value.Key}");
+                string month = human.birthday.Month.ToString();
+                if(data.ContainsKey(month))
+                {
+                    data[month].Add(human);
+                }
+                else
+                {
+                    List<Employee> employeeList = new List<Employee>();
+                    employeeList.Add(new Employee(human.name, human.birthday));
+                    data.Add(month, employeeList);
+                }
             }
         }
     }
